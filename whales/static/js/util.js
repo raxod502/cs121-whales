eb6405e4a182b6833483959ef15ea71c8c1082ed
max <you@example.com>
2019-04-24 23:35:16 -0700

    docstrings added to util

--- whales/static/js/util.js
+++ whales/static/js/util.js
@@ -1,4 +1,9 @@
 function apiRequest(request, callback) {
+  /**
+   * Handle api requests.
+   * Allows two inputs, request and callback.
+   * If request is valid, then pass and evaluate callback.
+   */
   return $.ajax({
     method: "POST",
     url: "/api/v1/http",
@@ -13,6 +18,9 @@ function apiRequest(request, callback) {
 }
 
 function decodeHash(hash) {
+  /**
+   * Decode hash
+   */
   const result = {};
   for (let component of decodeURI(hash).split(",")) {
     let [key, value] = component.split(":");
@@ -25,6 +33,9 @@ function decodeHash(hash) {
 }
 
 function encodeHash(hash) {
+  /**
+   * Encode hash
+   */
   return encodeURI(
     Object.entries(hash)
       .map(mapping => mapping.join(":"))
@@ -32,6 +43,7 @@ function encodeHash(hash) {
   );
 }
 
+//Below handles clicks on screen, hasing the location
 $(".homeLink").each((index, value) => {
   $(value).on("click", () => {
     window.location.href = "/" + window.location.hash;
